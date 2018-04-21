aaf1a257e351e07633fd48b02b788e8191ccfc58
Evan Willhite <evanmwillhite@562168.no-reply.drupal.org>
2018-04-21 10:22:15 -0500

    favicon , head

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -2,13 +2,27 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import '../assets/scss/init.scss'
 
+import favicon from './favicon.ico'
+
 class Layout extends React.Component {
   render() {
     const { children } = this.props
 
     return (
       <div className="layout">
-        <Helmet defaultTitle="Blog by Amanda Willhite" />
+        <Helmet
+          defaultTitle="Blog by Amanda Willhite"
+          meta={[
+            { name: 'description', content: 'The blog of Amanda Willhite' },
+          ]}
+          link={[
+            {
+              rel: 'icon',
+              type: 'image/x-icon',
+              href: favicon,
+            },
+          ]}
+        />
         {children()}
       </div>
     )
